7ec1cdb08034c0f2e90529afcc1a8b09f04f6dfa
Amer Klinic <aklinic1@etf.unsa.ba>
2019-10-25 16:02:31 +0200

    dodani testovi+override metode

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,9 +1,26 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Objects;
+
 public class Artikl {
     private String naziv;
     private double cijena;
     private String kod;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Artikl artikl = (Artikl) o;
+        return Double.compare(artikl.cijena, cijena) == 0 &&
+                Objects.equals(naziv, artikl.naziv) &&
+                Objects.equals(kod, artikl.kod);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(naziv, cijena, kod);
+    }
 
     public Artikl(String naziv, double cijena, String kod){
         this.naziv=naziv;
